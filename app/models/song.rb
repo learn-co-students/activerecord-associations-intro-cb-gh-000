210f99c3ad405e83f6add1d397946f474df1c995
Hayden Betts <haydenbetts@gmail.com>
2017-08-30 18:21:27 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
-
+  # note that here the symbol artist is singular
+  # not plural. So AR is associating this symbol
+  # with the model Artist.
+  belongs_to :artist
+  belongs_to :genre
 end
